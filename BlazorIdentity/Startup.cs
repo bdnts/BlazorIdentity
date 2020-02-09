441117680ed4092ab601876f1059501565ff1bee
Brian Douglass <corp@tps-eoltp.com>
2020-02-09 15:08:08 -0800

    Compared to Blazor app with Identity added made the following changes --Added RevalidatingIdentityAuthenticationStateProvider.cs --Added LoginDisplay.razor --Modified Startup.cs ----Added a bunch of references ----Added the AuthenticationStateProvider ----Database Error page ----Routing ----Authorization --Installed Diagnostics EntityFrameworkCore package --Added CascadingAuthenticationState to App.razor ----Added AuthorizeRouteView --In MainLayout added auth class and LoginDisplay --Completely changed Index according to example in aspnet.core.security.blazor, then modified to display results on screen.

--- BlazorIdentity/Startup.cs
+++ BlazorIdentity/Startup.cs
@@ -4,11 +4,16 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using BlazorIdentity.Areas.Identity;
 using BlazorIdentity.Data;
 
 namespace BlazorIdentity
@@ -28,6 +33,7 @@ namespace BlazorIdentity
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
+            services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
             services.AddSingleton<WeatherForecastService>();
         }
 
@@ -37,6 +43,7 @@ namespace BlazorIdentity
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
             }
             else
             {
@@ -47,9 +54,11 @@ namespace BlazorIdentity
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-            app.UseAuthentication();
 
             app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
