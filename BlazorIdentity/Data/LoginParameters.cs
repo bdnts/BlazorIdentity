dc6daf1d6f8d0c9a9010b49e6575b42c9d2fd18d
Brian Douglass <corp@tps-eoltp.com>
2020-03-14 01:45:17 -0700

    --Added model for use with form

---
+++ BlazorIdentity/Data/LoginParameters.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorIdentity.Data
+{
+    public class LoginParameters
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+}
+
