48b79d631fbe5a7a5b96c03d028e0dae55a5325b
Brian Douglass <corp@tps-eoltp.com>
2020-03-14 17:31:07 -0700

    --Added the models

---
+++ BlazorIdentity/Models/UserInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorIdentity.Models
+{
+    public class UserInfo
+    {
+        public bool IsAuthenticated { get; set; }
+        public string UserName { get; set; }
+        public Dictionary<string, string> ExposedClaims { get; set; }
+    }
+}
