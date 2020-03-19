a6f9f49424d5926b60cea8f8a55fce46dc5f59df
Brian Douglass <corp@tps-eoltp.com>
2020-03-19 12:02:56 -0700

    --Added ServerSideValidator to display per field messages

---
+++ BlazorIdentity/Helpers/ServerSideValidator.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorIdentity.Helpers
+{
+    public class ServerSideValidator : ComponentBase
+    {
+        private ValidationMessageStore _messageStore;
+
+        [CascadingParameter] EditContext CurrentEditContext { get; set; }
+
+        /// <inheritdoc />  
+        protected override Task OnParametersSetAsync()
+        {
+            if (CurrentEditContext == null)
+            {
+                throw new InvalidOperationException($"{nameof(ServerSideValidator)} requires a cascading " +
+                    $"parameter of type {nameof(EditContext)}. For example, you can use {nameof(ServerSideValidator)} " +
+                    $"inside an {nameof(EditForm)}.");
+            }
+
+            _messageStore = new ValidationMessageStore(CurrentEditContext);
+            CurrentEditContext.OnValidationRequested += (s, e) => _messageStore.Clear();
+            CurrentEditContext.OnFieldChanged += (s, e) => _messageStore.Clear(e.FieldIdentifier);
+            return base.OnParametersSetAsync();
+        }
+
+        public void DisplayErrors(Dictionary<string, List<string>> errors)
+        {
+            foreach (var err in errors)
+            {
+                _messageStore.Add(CurrentEditContext.Field(err.Key), err.Value);
+            }
+            CurrentEditContext.NotifyValidationStateChanged();
+        }
+    }
+}
